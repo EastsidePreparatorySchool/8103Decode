b3f690fdfd8982504f027910c85efb75b199e889
Victoria Zhang <136418275+VictoriaWZh@users.noreply.github.com>
2025-10-09 15:34:39 -0700

    removed comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/Common.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/Common.java
@@ -6,9 +6,6 @@ import com.acmerobotics.dashboard.config.Config;
 public class Common {
     public static double INTAKE_FORWARD_POWER = 1.0;
     public static double INTAKE_BACKWARD_POWER = -1.0;
-//    public static double SPINDEXER_COUNTERCLOCKWISE_POWER = -1.0;
-//    public static double SPINDEXER_CLOCKWISE_POWER = 1.0;
-//    public static double SPINDEXER_NO_POWER = 0.0;
     public static double SPINDEXER_SLOT1TRANSFER_DIRECTION = 0.0; //TBD
     public static double SPINDEXER_SLOT1OPEN_DIRECTION = 0.0;
     public static double SPINDEXER_SLOT2TRANSFER_DIRECTION = 0.0;
