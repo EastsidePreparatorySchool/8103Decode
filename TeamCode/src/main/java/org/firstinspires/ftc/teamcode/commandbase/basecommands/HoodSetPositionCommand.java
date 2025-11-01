262b777215ccb3a10de039b60eabe81637981953
thexpiredpear <49598383+thexpiredpear@users.noreply.github.com>
2025-11-01 15:43:29 -0700

    rename hood cmd

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandbase/basecommands/HoodSetPositionCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandbase/basecommands/HoodSetPositionCommand.java
@@ -1,15 +1,15 @@
-package org.firstinspires.ftc.teamcode.commandbase.safecommands;
+package org.firstinspires.ftc.teamcode.commandbase.basecommands;
 
 import com.seattlesolvers.solverslib.command.InstantCommand;
 import org.firstinspires.ftc.teamcode.lib.RobotHardware;
 import org.firstinspires.ftc.teamcode.subsystems.HoodSubsystem;
 
-public class ShooterSetHoodPositionCommand extends InstantCommand {
-    public ShooterSetHoodPositionCommand(HoodSubsystem hoodSubsystem, double position) {
+public class HoodSetPositionCommand extends InstantCommand {
+    public HoodSetPositionCommand(HoodSubsystem hoodSubsystem, double position) {
         super(() -> hoodSubsystem.setHoodPosition(position));
     }
 
-    public ShooterSetHoodPositionCommand(double position) {
+    public HoodSetPositionCommand(double position) {
         this(RobotHardware.getInstance().hoodSubsystem, position);
     }
 }
